a54949ab7c099c6c170e802185c65acb4b3c1982
Zaki <muhammadfarid.zaki@gmail.com>
2023-05-14 18:40:01 +0700

    installing packages

--- android/app/src/main/java/com/pintarapp/MainActivity.java
+++ android/app/src/main/java/com/pintarapp/MainActivity.java
@@ -1,4 +1,6 @@
 package com.pintarapp;
+import android.os.Bundle;
+
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -15,7 +17,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "PintarApp";
   }
-
+@Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
